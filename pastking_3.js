2661b37cc66d3f91e6ea3e638efef862cd739fef
DreamofInfinity1 <129919780+DreamofInfinity1@users.noreply.github.com>
2025-03-27 12:35:02 +0800

    Update pastking_4.js

--- pastking_3.js
+++ pastking_3.js
@@ -33,7 +33,7 @@ function ChucklePostAI(AI_option) {
     const style = document.createElement('style');
     style.textContent = `
       .post-ai {
-        font-family: 'Maple Mono CN ExtraBold', sans-serif;
+        font-family: 'STDongGuanTi', sans-serif;
         margin-bottom: 20px;
       }
       .ai-container {
